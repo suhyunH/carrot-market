a9359123a8f28a6fd79601818358b7cda29e1b33
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-04-22 16:03:21 +0900

    account logic before clean it up

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,7 +4,44 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 
 async function handler(req:NextApiRequest, res:NextApiResponse){
-    console.log(req.body);
+    const {phone, email} = req.body;
+    let user;
+    if(email){
+        user = await client.user.findUnique({
+            where:{
+                email:email //email,
+            }
+        });
+        if(user) console.log("found it");
+        if(!user){
+            console.log("didnt find, will create")
+           user = await client.user.create({
+                data:{
+                    name:"Anonymous",
+                    email,
+                }
+            })
+        }
+        console.log(user);
+    }
+    if(phone){
+        user = await client.user.findUnique({
+            where:{
+                phone: +phone,//number로 변경해줌 string으로 바꿔주고 싶으면 +""
+            }
+        });
+        if(user) console.log("found it");
+        if(!user){
+            console.log("didnt find, will create")
+           user = await client.user.create({
+                data:{
+                    name:"Anonymous",
+                    phone:+phone, 
+                }
+            })
+        }
+        console.log(user);
+    }
     return res.status(200).end();
 }
 export default withHandler("POST", handler);
