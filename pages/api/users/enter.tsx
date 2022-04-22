0227e7d2f63046bd7fed858b55f163fcb56d7b6f
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-04-22 18:49:27 +0900

    clean up token logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,23 +1,13 @@
 import client from '@libs/server/client';
-import withHandler from '@libs/server/withHandler';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-async function handler(req:NextApiRequest, res:NextApiResponse){
+async function handler(req:NextApiRequest, res:NextApiResponse<ResponseType>){
     const {phone, email} = req.body;
-    const user = phone? {phone:+phone} : {email}
-    const payload = Math.floor(100000+Math.random()*900000)+"";
-    // const user = await client.user.upsert({
-    //     where:{
-    //     //    ...(phone&&{phone:+phone}),
-    //     //    ...(email&&{email,}),
-    //           ...payload
-    //     },
-    //     create:{
-    //         name:"Anonymous",
-    //         ...payload
-    //     },
-    //     update:{}
-    // });
+    const user = phone? {phone:+phone} : email?{email}:null;
+    if(!user) return res.status(400).json({ok:false})
+    const payload = Math.floor(100000+Math.random()*900000)+""; //+""은 number를 string으로 변환해줌. 
+    
     const token = await client.token.create({
         data:{
             payload,
@@ -35,6 +25,8 @@ async function handler(req:NextApiRequest, res:NextApiResponse){
         }
     });
     console.log(token);
-    return res.status(200).end();
+    return res.json({
+        ok:true,
+    });
 }
 export default withHandler("POST", handler);
