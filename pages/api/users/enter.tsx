2304c986122facc7e3fc040bfc42e35a4d8b6bf6
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-04-22 18:42:49 +0900

    token logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,23 +2,39 @@ import client from '@libs/server/client';
 import withHandler from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-
 async function handler(req:NextApiRequest, res:NextApiResponse){
     const {phone, email} = req.body;
-    const payload = phone? {phone:+phone} : {email}
-    const user = await client.user.upsert({
-        where:{
-        //    ...(phone&&{phone:+phone}),
-        //    ...(email&&{email,}),
-              ...payload
-        },
-        create:{
-            name:"Anonymous",
-            ...payload
-        },
-        update:{}
+    const user = phone? {phone:+phone} : {email}
+    const payload = Math.floor(100000+Math.random()*900000)+"";
+    // const user = await client.user.upsert({
+    //     where:{
+    //     //    ...(phone&&{phone:+phone}),
+    //     //    ...(email&&{email,}),
+    //           ...payload
+    //     },
+    //     create:{
+    //         name:"Anonymous",
+    //         ...payload
+    //     },
+    //     update:{}
+    // });
+    const token = await client.token.create({
+        data:{
+            payload,
+            user:{
+                connectOrCreate:{
+                    where:{
+                            ...user
+                        },
+                    create:{
+                            name:"Anonymous",
+                            ...user
+                        },
+                }
+            }
+        }
     });
-    console.log(user);
+    console.log(token);
     return res.status(200).end();
 }
 export default withHandler("POST", handler);
