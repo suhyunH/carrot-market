3231a725198a430bf5430196d82fa28d007dcbca
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-04-22 16:19:30 +0900

    account login clean up with payload

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,43 +5,20 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req:NextApiRequest, res:NextApiResponse){
     const {phone, email} = req.body;
-    let user;
-    if(email){
-        user = await client.user.findUnique({
-            where:{
-                email:email //email,
-            }
-        });
-        if(user) console.log("found it");
-        if(!user){
-            console.log("didnt find, will create")
-           user = await client.user.create({
-                data:{
-                    name:"Anonymous",
-                    email,
-                }
-            })
-        }
-        console.log(user);
-    }
-    if(phone){
-        user = await client.user.findUnique({
-            where:{
-                phone: +phone,//number로 변경해줌 string으로 바꿔주고 싶으면 +""
-            }
-        });
-        if(user) console.log("found it");
-        if(!user){
-            console.log("didnt find, will create")
-           user = await client.user.create({
-                data:{
-                    name:"Anonymous",
-                    phone:+phone, 
-                }
-            })
-        }
-        console.log(user);
-    }
+    const payload = phone? {phone:+phone} : {email}
+    const user = await client.user.upsert({
+        where:{
+        //    ...(phone&&{phone:+phone}),
+        //    ...(email&&{email,}),
+              ...payload
+        },
+        create:{
+            name:"Anonymous",
+            ...payload
+        },
+        update:{}
+    });
+    console.log(user);
     return res.status(200).end();
 }
 export default withHandler("POST", handler);
