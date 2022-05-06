346580b022166a37821e8c88ef4f1d5d6d2854df
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-05-06 23:14:11 +0900

    serverless sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,3 +1,4 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import client from '@libs/server/client';
 import withHandler, { ResponseType } from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
@@ -7,8 +8,20 @@ async function handler(
     req:NextApiRequest, res:NextApiResponse<ResponseType>
     ){
     const {token} = req.body;
-    console.log(token)
+    const exists = await client.token.findUnique({where:{
+        payload:token,
+    },
+    // include:{user:true},
+    });
+    if(!exists) res.status(404).end();
+    req.session.user ={
+        id:exists?.userId
+    }
+    await req.session.save()
     res.status(200).end();
    
 }
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler),{
+    cookieName:"carrotsession",
+    password:"192932932asldjasldjasdaldajdladjlaksdjalakjdlkasj18381238124812313"
+});
