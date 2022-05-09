678276c9d93664565a216891f511cfd44ae63f14
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-05-09 22:39:13 +0900

    fav Button part2

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -7,7 +7,10 @@ import { withApiSession } from '@libs/server/withSession';
 async function handler(
     req:NextApiRequest, res:NextApiResponse<ResponseType>
     ){
-        const {id} = req.query;
+        const {
+            query:{id},
+            session:{user}
+        } = req;
         const product = await client.product.findUnique({
             where:{
                 id : +id.toString(), 
@@ -37,9 +40,19 @@ async function handler(
                 }
             },
         }) 
+        const isLiked =Boolean(await client.fav.findFirst({
+            where:{
+                productId:product?.id,
+                userId: user?.id
+            },
+            select:{
+                id:true,
+            }
+        }));
         res.json({
             ok:true,
             product,
+            isLiked,
             relatedProducts
         })
 }
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import Link from 'next/link';
 import { Product, User } from '@prisma/client';
+import useMutation from '@libs/client/useMutation';
+import { cls } from '@libs/client/utils';
 
 interface ProductWithUser extends Product{
   user: User
@@ -13,11 +15,18 @@ interface ItemDetailResponse{
   ok:boolean;
   product:ProductWithUser;
   relatedProducts: Product[];
+  isLiked : boolean;
 }
 
 const ItemDetail: NextPage=()=>{
   const router = useRouter();
   const {data} =useSWR<ItemDetailResponse>(router.query.id? `/api/products/${router.query.id}` : null)
+  const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`)
+
+  const onFavClick=()=>{
+    toggleFav({});
+  }
+
  return(
    <Layout canGoBack>
 
@@ -48,7 +57,15 @@ const ItemDetail: NextPage=()=>{
           }
           <div className='flex items-center justify-between space-x-2'>
             <Button text='Talk to seller' large/>
-            <button className='p-3 flex items-center text-gray-400 hover:bg-gray-100 hover:text-gray-500 rounded-md'>
+            <button onClick={onFavClick}  className={cls('p-3 flex items-center  hover:bg-gray-100  rounded-md', data?.isLiked?"text-red-400 hover:text-red-500" :"text-gray-400 hover:text-gray-500")}>
+              {data?.isLiked?(
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                <path 
+                fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+                </svg>
+              ):
+              
               <svg
                 className="h-6 w-6 "
                 xmlns="http://www.w3.org/2000/svg"
@@ -64,6 +81,7 @@ const ItemDetail: NextPage=()=>{
                   d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
                 />
               </svg>
+              }
             </button>
           </div>
         </div>
